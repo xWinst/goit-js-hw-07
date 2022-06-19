84d1641c1dadb78bf53f525c7a6d9c79260dc4b9
Oleg <hellreebok@gmail.com>
2022-06-19 15:35:09 +0300

    Task is done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+gallery.addEventListener("click", selectImage)
+
+
+let galleryItem;
+let link;
+let image;
+
+for (let picture of galleryItems) {
+    galleryItem = document.createElement("div");
+    galleryItem.classList.add("gallery__item");
+
+    link = document.createElement("a");
+    link.href = picture.original;
+    link.classList.add("gallery__link");
+    link.preve
+
+    image = document.createElement("img");
+    image.src = picture.preview;
+    image.alt = picture.description;
+    image.setAttribute("data-source", picture.original);
+    image.classList.add("gallery__image"); 
+
+    link.append(image);
+    galleryItem.append(link);
+    gallery.append(galleryItem);
+  }
+
+
+  function selectImage(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") return;
+    const instance = basicLightbox.create(`<img src="${event.target.dataset.source}">`, {
+        
+        onShow: () => {
+            
+            window.addEventListener("keydown", clickEsc);
+        },
+        
+        onClose: () => window.removeEventListener("keydown", clickEsc)}
+        );
+        
+        function clickEsc(event) {
+            if (event.key === "Escape") {
+                instance.close();
+              }
+          }
+    instance.show();
+  }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+let markup = "";
+
+for (let picture of galleryItems) {
+
+    markup +=  `<a class="gallery__item" href="${picture.original}">
+                    <img
+                        class="gallery__image"
+                        src="${picture.preview}"
+                        data-source="${picture.original}"
+                        alt="${picture.description}"
+                    />
+                </a>`;
+  }
+
+gallery.insertAdjacentHTML("beforeend", markup); //А как же то, что в <ul> дети могут быть только <li>?
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt', captionDelay: 250, captionPosition: 'bottom'});
